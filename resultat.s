b01cb525cbcf19416ae4e9fed7511716b385568d
caballoj <julien.caballol@hotmail.fr>
2015-04-15 16:17:20 +0200

    test

--- resultat.s
+++ resultat.s
@@ -1,13 +1,13 @@
-	.file	"resultat.ll"
 	.text
+	.file	"resultat04_autreReactionBizarre.ll"
 	.globl	definir
 	.align	16, 0x90
 	.type	definir,@function
 definir:                                # @definir
 	.cfi_startproc
 # BB#0:
-	movl	%ecx, %eax
-	ret
+	movl	%edi, %eax
+	retq
 .Ltmp0:
 	.size	definir, .Ltmp0-definir
 	.cfi_endproc
@@ -18,56 +18,60 @@ definir:                                # @definir
 main:                                   # @main
 	.cfi_startproc
 # BB#0:
-	pushl	%ebx
-.Ltmp5:
-	.cfi_def_cfa_offset 8
-	pushl	%edi
-.Ltmp6:
-	.cfi_def_cfa_offset 12
-	pushl	%esi
-.Ltmp7:
+	pushq	%rbp
+.Ltmp1:
 	.cfi_def_cfa_offset 16
-	subl	$16, %esp
-.Ltmp8:
+	pushq	%r14
+.Ltmp2:
+	.cfi_def_cfa_offset 24
+	pushq	%rbx
+.Ltmp3:
 	.cfi_def_cfa_offset 32
-.Ltmp9:
-	.cfi_offset %esi, -16
-.Ltmp10:
-	.cfi_offset %edi, -12
-.Ltmp11:
-	.cfi_offset %ebx, -8
-	movl	$15, 12(%esp)
-	movl	$15, (%esp)
-	calll	definir
-	movl	%eax, %esi
-	movl	$15, (%esp)
-	calll	definir
-	movl	%eax, %edi
-	movb	$0, 11(%esp)
-	movb	$10, 10(%esp)
-	movb	$100, 9(%esp)
-	movb	$37, 8(%esp)
-	leal	8(%esp), %ebx
-	movl	%ebx, (%esp)
-	movl	$15, 4(%esp)
-	calll	printf
-	movl	%ebx, (%esp)
-	movl	$15, 4(%esp)
-	calll	printf
-	movl	%edi, 4(%esp)
-	movl	%ebx, (%esp)
-	calll	printf
-	movl	%esi, 4(%esp)
-	movl	%ebx, (%esp)
-	calll	printf
+	subq	$16, %rsp
+.Ltmp4:
+	.cfi_def_cfa_offset 48
+.Ltmp5:
+	.cfi_offset %rbx, -32
+.Ltmp6:
+	.cfi_offset %r14, -24
+.Ltmp7:
+	.cfi_offset %rbp, -16
+	movl	$15, 12(%rsp)
+	movl	$15, %edi
+	callq	definir
+	movl	%eax, %r14d
+	movl	$15, %edi
+	callq	definir
+	movl	%eax, %ebp
+	movb	$0, 11(%rsp)
+	movb	$10, 10(%rsp)
+	movb	$100, 9(%rsp)
+	movb	$37, 8(%rsp)
+	leaq	8(%rsp), %rbx
+	movl	$15, %esi
 	xorl	%eax, %eax
-	addl	$16, %esp
-	popl	%esi
-	popl	%edi
-	popl	%ebx
-	ret
-.Ltmp12:
-	.size	main, .Ltmp12-main
+	movq	%rbx, %rdi
+	callq	printf
+	movl	$15, %esi
+	xorl	%eax, %eax
+	movq	%rbx, %rdi
+	callq	printf
+	xorl	%eax, %eax
+	movq	%rbx, %rdi
+	movl	%ebp, %esi
+	callq	printf
+	xorl	%eax, %eax
+	movq	%rbx, %rdi
+	movl	%r14d, %esi
+	callq	printf
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbx
+	popq	%r14
+	popq	%rbp
+	retq
+.Ltmp8:
+	.size	main, .Ltmp8-main
 	.cfi_endproc
 
 
